8954b05f0ce2d8f47c0175eaf58aa513010ac3af
damayantiazure <92169356+damayantiazure@users.noreply.github.com>
2024-01-05 09:55:35 +0100

    Update deploy.sh

--- src/Infrastructure/Containers/deploy.sh
+++ src/Infrastructure/Containers/deploy.sh
@@ -2,7 +2,7 @@
 
 export resourceGroupName="APIM-DEVOPS-Coantapp"
 export location="westeurope"
-export APP_NAME="solardemo2"
+export APP_NAME="solardemo1"
 export APP_ENV="dev"
 export TAG_NAME="beta2"
 
@@ -10,4 +10,4 @@ echo "Starting deploying the app provisioning..."
 
 
 echo "Deploying app Bicep file..."
-az deployment group create --resource-group $resourceGroupName --template-file 'apps.bicep'  --parameters app.bicepparam
+az deployment group create --resource-group $resourceGroupName --template-file 'app.bicep'  --parameters app.bicepparam
